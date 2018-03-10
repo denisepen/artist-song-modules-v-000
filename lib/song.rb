4d6cd4adfb4908d0f4ced0af156593870e1b1673
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 17:20:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
 
   @@all = []
 
-  def initialize
-    self.class.all << self
-  end
+  # def initialize
+  #   self.class.all << self
+  # end
 
   # def self.find_by_name(name)
   #   @@all.detect{|a| a.name == name}
