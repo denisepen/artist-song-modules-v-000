64925bec9f54316441dcdc2cd9aa9bed303d1eb5
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 17:19:29 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -4,12 +4,12 @@ module Memorable
     def reset_all
       self.all.clear
     end
- 
+
     def count
       self.all.count
     end
   end
- 
+
   module InstanceMethods
     def initialize
       # some more code coming soon!
